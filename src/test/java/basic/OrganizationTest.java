b6d7d1b4e4de4ca7267abba8ee7507c1cf35c9cf
Dell <Dell@DESKTOP-7CSKUVA>
2024-03-18 17:56:27 +0530

    updated

--- src/test/java/basic/OrganizationTest.java
+++ src/test/java/basic/OrganizationTest.java
@@ -1,14 +1,47 @@
 package basic;
 
+import java.io.IOException;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import common_utils.PropertyFileUtil;
+import common_utils.WebDriverUtil;
+
+
 public class OrganizationTest {
 	@Test
-	public void createOrganizationsTest() {
-		//launch the browser
-		WebDriver driver=new ChromeDriver();
-		driver.get("http://localhost:8888/");
+	public void createOrganizationsTest() throws IOException, InterruptedException {
+		
+		PropertyFileUtil propertyFileUtil=new PropertyFileUtil();
+		WebDriverUtil webDriverUtil=new WebDriverUtil();
+		
+			WebDriver driver=new ChromeDriver();
+			//to maximize the window
+			webDriverUtil.maximize(driver);
+			//to apply wait
+			webDriverUtil.implicitWait(driver);
+			
+			//to read data from property file
+			String URL = propertyFileUtil.getDataFromPropertyFile("Url");		
+			String USERNAME = propertyFileUtil.getDataFromPropertyFile("Username");
+			String PASSWORD = propertyFileUtil.getDataFromPropertyFile("Password");
+			
+			//launch application
+			driver.get(URL);
+			
+			Thread.sleep(2000);
+			//enter username in username text fiels
+			driver.findElement(By.name("user_name")).sendKeys(USERNAME);
+			
+			Thread.sleep(2000);
+			//enter password in password text field
+			driver.findElement(By.name("user_password")).sendKeys(PASSWORD);
+			
+			Thread.sleep(2000);
+			//click on login button
+			driver.findElement(By.id("submitButton")).click();
 	}
 }
